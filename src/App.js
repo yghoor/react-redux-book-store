e11584f290659745f172ad251b4f58f22cee4d55
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-02 15:13:20 +0200

    Render 'Navbar' inside App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.js';
 import Books from './pages/Books.js';
 import Categories from './pages/Categories.js';
 import './App.css';
@@ -8,6 +9,7 @@ import './App.css';
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="categories" element={<Categories />} />
