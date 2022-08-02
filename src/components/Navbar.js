ed9d2bb3d80599ee3b24b5db734b651c0a64ed23
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-02 15:12:56 +0200

    Create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <header>
+    <nav className="page-navbar">
+      <h1>Bookstore</h1>
+
+      <ul className="nav-links">
+        <li>
+          <NavLink to="/" activeclassname="active" >BOOKS</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/categories" activeclassname="active" >CATEGORIES</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navbar;
