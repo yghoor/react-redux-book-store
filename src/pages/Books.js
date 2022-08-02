46fc5414e71492f23a3ad8b07efb64b4ccf15727
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-02 15:10:16 +0200

    Create 'Books' page component

---
+++ src/pages/Books.js
@@ -0,0 +1,12 @@
+import Booklist from '../components/Booklist.js';
+import BookForm from '../components/BookForm.js';
+
+const Books = () => (
+  <div className="books-page">
+    <Booklist />
+    <BookForm />
+  </div>
+
+);
+
+export default Books;
