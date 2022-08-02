448daa9b4e5a9671976c094643347878b763cc44
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-02 15:10:48 +0200

    Create 'Categories' page component

---
+++ src/pages/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div className="categories-page">
+      <h1>Under construction</h1>
+    </div>
+  );
+}
+
+export default Categories;
