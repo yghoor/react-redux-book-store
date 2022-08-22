d0f2eb48da6ec4aba434d96a5639d485c119cc4a
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-23 00:07:07 +0200

    Add thunk middleware to app

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,16 @@
-import { combineReducers, applyMiddleware } from 'redux';
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
 });
 
-const store = configureStore(
-  { reducer },
-  applyMiddleware(logger),
-);
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger),
+});
 
 export default store;
