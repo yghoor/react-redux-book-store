97ff90b71032a9872b5f6afb5d1f5d3d03ea9088
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-23 00:32:00 +0200

    Remove React.StrictMode due to error

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </>,
 );
