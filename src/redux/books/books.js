0a631ceb795ec888c35bb885f84a45d06efae3b6
Yahya Ghoor <ghooryahya@gmail.com>
2022-08-23 00:14:48 +0200

    Build action creator to retrieve books from API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,4 @@
+const RETRIEVE_BOOKS = 'react-redux-book-store/books/RETRIEVE_BOOKS';
 const ADD_BOOK = 'react-redux-book-store/books/ADD_BOOK';
 const REMOVE_BOOK = 'react-redux-book-store/books/REMOVE_BOOK';
 
@@ -16,8 +17,24 @@ const reducer = (state = initialState, action) => {
 
 export default reducer;
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
+export const retrieveBooks = () => async (dispatch) => {
+  const books = await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/3eIgjc1cLtb5eamuunbl/books',
+  )
+    .then((response) => response.json());
+
+  if (Object.keys(books).length === 0) {
+    dispatch({
+      type: NO_BOOKS_IN_STORE,
+    });
+  } else {
+    dispatch({
+      type: RETRIEVE_BOOKS,
+      payload: books,
+    });
+  }
+};
+
   payload,
 });
 
